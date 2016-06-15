418b481b47fedc3dbeaa531303bfa52261df43b4
Luis Manotas <lgerman@gmail.com>
2016-06-15 13:54:34 +0200

    Showing Hotels list when search option by location

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,2 +1,11 @@
 class HotelsController < ApplicationController
+  def index
+    @hotels = HotelSearchService.near(search_hotels_params)
+  end
+
+private
+  
+  def search_hotels_params
+    params.permit(:lat, :lon)
+  end
 end
--- spec/acceptance/search_for_hotel_spec.rb
+++ spec/acceptance/search_for_hotel_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 feature 'Search for Hotel near me', type: :feature do
+  use_vcr_cassette "hotels_list"
+
   scenario 'User Clicks on Search button' do
     visit '/'
 
     click_button 'Search for Hotels'
 
     expect(page).to have_content('Results found')
+    expect(page).to have_selector('table.hotel-list-table')
   end
 end
