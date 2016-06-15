bbf0394d33d84fb79d8c1e487443ac7cf854b901
Luis Manotas <lgerman@gmail.com>
2016-06-15 07:15:17 +0200

    HotelsSearchService using Places client to bring list of hotels

--- spec/services/hotel_search_service_spec.rb
+++ spec/services/hotel_search_service_spec.rb
@@ -6,17 +6,11 @@ describe HotelSearchService do
   end
 
   context 'Using GooglePlaces Library' do
-    it do
-      VCR.use_cassette("hotels_list") do
-        # expect_any_instance_of(GooglePlaces).to receive(:spots).with(my_location[:lat], my_location[:lon], name: 'hotels')
-        described_class.near(my_location)
-      end
+    subject(:hotels_near_me){ described_class.near(my_location) }
+
+    specify do
+      expect_any_instance_of(GooglePlaces::Client).to receive(:spots).with(my_location[:lat], my_location[:lon], name: 'hotels')
+      hotels_near_me
     end
-  end
-
-  context 'Using location to bring list of hotels' do
-    subject { described_class.near() }
-
-    it 'list of hotels'
   end
 end
