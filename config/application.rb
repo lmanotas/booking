d3a1250d9bbb3ef70aec5ef302dc4e1007916013
Luis Manotas <lgerman@gmail.com>
2016-06-15 05:54:11 +0200

    Adding app/services to autoload paths

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module ConichiBooking
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # Autoloading app/services dir
+    config.autoload_paths += %W(#{config.root}/app/services)
   end
 end
