2ed08231198e88ae3fec91fe3aaaab1eb20c5223
Luis Manotas <lgerman@gmail.com>
2016-06-15 07:31:44 +0200

    Configurable Search Criteria

--- app/services/hotel_search_service.rb
+++ app/services/hotel_search_service.rb
@@ -1,5 +1,7 @@
 class HotelSearchService
+  SEARCH_CRITERIA = 'hotels'
+
   def self.near(location = {})
-    GOOGLE_PLACES_CLIENT.spots(location[:lat], location[:lon], name: 'hotels')
+    GOOGLE_PLACES_CLIENT.spots(location[:lat], location[:lon], name: SEARCH_CRITERIA)
   end
 end
